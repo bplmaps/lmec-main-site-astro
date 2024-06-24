98eb88a44ca9cf8361aeeb7aaa48d184e605fe43
Alex Knoll <alex@agk.io>
2024-06-24 14:15:34 -0400

    add placeholder for  block

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ export default defineConfig({
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
       components: {
+        'alert-box': 'storyblok/AlertBox',
         'event-page': 'storyblok/EventPage',
         'standard-lmec-main-page': 'storyblok/StandardLmecMainPage',
       },
