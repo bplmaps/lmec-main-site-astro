2ff1ac35d5849f950f388d0b0d1cdccb7b537c72
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2026-02-23 11:52:58 -0500

    rework search, pt 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import { storyblok } from '@storyblok/astro';
 import { loadEnv } from 'vite';
 import netlify from '@astrojs/netlify';
+import svelte from '@astrojs/svelte';
 
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 const config = {
   integrations: [
+    svelte(),
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
       components: {
